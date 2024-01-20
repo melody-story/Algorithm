86fccb0d444b278155dafce5d2d4a3e26aefc06a
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-20 20:00:58 +0900

    Time: 13 ms (98.95%), Space: 73.2 MB (21.89%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -5,10 +5,10 @@ class Solution {
         for (int i = 0; i < n; i++) {
             xArray[i] = points[i][0];
         }
-        int[] array = Arrays.stream(xArray).sorted().toArray();
+        Arrays.sort(xArray);
         int max = 0;
         for (int i = 0; i < n - 1; i++) {
-            int result = array[i + 1] - array[i];
+            int result = xArray[i + 1] - xArray[i];
             if (result > max) max = result;
         }
         return max;
