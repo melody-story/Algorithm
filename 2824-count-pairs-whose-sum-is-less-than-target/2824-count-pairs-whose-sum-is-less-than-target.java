10bb506d3cd1ba77dfa999857f8efde7a930b480
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-21 19:51:31 +0900

    Time: 1 ms (100.00%), Space: 42.4 MB (20.07%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.java
@@ -1,12 +1,15 @@
-class Solution {
-    public int countPairs(List<Integer> nums, int target) {
-        int cnt =0;
-        Integer[] array = nums.toArray(new Integer[0]);
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i+1; j < array.length; j++) {
-                if (array[i] + array[j] < target) cnt++;
+    class Solution {
+        public int countPairs(List<Integer> nums, int target) {
+            int cnt =0;
+            int array[] = new int[nums.size()];
+            for(int i=0;i<nums.size();i++){
+                array[i]=nums.get(i);
             }
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i+1; j < array.length; j++) {
+                    if (array[i] + array[j] < target) cnt++;
+                }
+            }
+            return cnt;
         }
-        return cnt;
     }
-}
