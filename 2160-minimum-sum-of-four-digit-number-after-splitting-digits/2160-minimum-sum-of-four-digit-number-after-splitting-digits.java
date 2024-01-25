623389aeb02b1d68be2cf812d840a2e8d4e1e4b8
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-25 16:46:20 +0900

    Time: 1 ms (30.64%), Space: 40.8 MB (10.80%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -1,15 +1,11 @@
 class Solution {
     public int minimumSum(int num) {
         char[] charArray = new String(String.valueOf(num)).toCharArray();
-        List<Character> chars = new ArrayList<>(charArray.length);
+        List<Integer> chars = new ArrayList<>(charArray.length);
         for (char cha : charArray){
-            chars.add(cha);
+            chars.add(Character.getNumericValue(cha));
         }
-        chars.sort(Comparator.naturalOrder());
-        StringBuilder append = new StringBuilder(String.valueOf(chars.get(0))).append(chars.get(2));
-        Integer integer = new Integer(String.valueOf(append));
-        StringBuilder append1 = new StringBuilder(String.valueOf(chars.get(1))).append(chars.get(3));
-        Integer integer1 = new Integer(String.valueOf(append1));
-        return integer + integer1;
+        Collections.sort(chars);
+        return (chars.get(0) * 10 + chars.get(2)) + (chars.get(1) * 10 + chars.get(3));
     }
 }
