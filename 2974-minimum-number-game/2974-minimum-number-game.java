7fcb3616ba2c4b8b5f191b94d091bcbfc7d163ae
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-27 17:25:48 +0900

    Time: 2 ms (98.79%), Space: 44.5 MB (94.09%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.java
+++ 2974-minimum-number-game/2974-minimum-number-game.java
@@ -1,14 +1,10 @@
 class Solution {
     public int[] numberGame(int[] nums) {
-        List<Integer> list = new ArrayList<>(100);
-        for (int n :nums) {
-            list.add(n);
-        }
-        list.sort(Comparator.naturalOrder());
-
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length; i+=2) {
-            nums[i] = list.get(i+1);
-            nums[i+1] = list.get(i);
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
         }
         return nums;
     }
