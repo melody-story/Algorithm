3084d05701f9451b9345e78519c11b636273421a
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-30 17:45:44 +0900

    Time: 2 ms (9.82%), Space: 42 MB (18.46%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> target = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            target.add(index[i],nums[i]);
+        }
+        Integer[] array = target.toArray(new Integer[0]);
+        return Arrays.stream(array).mapToInt(i->i).toArray();
+    }
+}
