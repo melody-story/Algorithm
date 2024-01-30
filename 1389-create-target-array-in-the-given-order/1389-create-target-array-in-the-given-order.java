e7c8c75977aae4fd6357affd4fe9e919e4a1c0d4
손명희 <77363145+melody-story@users.noreply.github.com>
2024-01-30 17:48:48 +0900

    Time: 0 ms (100.00%), Space: 41.5 MB (63.94%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,10 +1,18 @@
-class Solution {
-    public int[] createTargetArray(int[] nums, int[] index) {
-        List<Integer> target = new ArrayList<>(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            target.add(index[i],nums[i]);
+    class Solution {
+        public int[] createTargetArray(int[] nums, int[] index) {
+            List<Integer> target = new ArrayList<>(nums.length);
+            for (int i = 0; i < nums.length; i++) {
+                target.add(index[i],nums[i]);
+            }
+            
+            // 추가 메모리를 차지하는 객체를 생성하게 됨.
+//            Integer[] array = target.toArray(new Integer[0]);
+//            return Arrays.stream(array).mapToInt(i->i).toArray();
+
+            // 기존 공간을 사용함으로써 불필요한 공간 생기지 않도록 함.
+            for(int i = 0; i < nums.length; i++){
+                nums[i] = target.get(i);
+            }
+            return nums;
         }
-        Integer[] array = target.toArray(new Integer[0]);
-        return Arrays.stream(array).mapToInt(i->i).toArray();
     }
-}
