1df4048c9353a07073866e239e51652082c7c649
손명희 <77363145+melody-story@users.noreply.github.com>
2024-02-01 23:49:47 +0900

    Time: 1 ms (99.70%), Space: 41.2 MB (10.01%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,11 +1,32 @@
 class Solution {
     public int sumOfMultiples(int n) {
         int sum = 0;
-        for (int i = n; i >= 3; i--) {
-            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
-                sum+=i;
+        boolean visited[] = new boolean[n + 1];
+
+        // Find the Numbers Divisible by 3
+        for(int i = 3; i <= n; i += 3) {
+            if(!visited[i]) {
+                sum += i;
+                visited[i] = true;
             }
         }
+
+        // Find the Numbers Divisible by 5
+        for(int i = 5; i <= n; i += 5) {
+            if(!visited[i]) {
+                sum += i;
+                visited[i] = true;
+            }
+        }
+
+        // Find the Numbers Divisible by 7
+        for(int i = 7; i <= n; i += 7) {
+            if(!visited[i]) {
+                sum += i;
+                visited[i] = true;
+            }
+        }
+
         return sum;
     }
 }
