9c9c8879dee195d1113f06bf4c5a1a4d7fef6815
손명희 <77363145+melody-story@users.noreply.github.com>
2024-02-02 14:14:12 +0900

    Time: 1 ms (81.27%), Space: 44.3 MB (62.54%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -2,14 +2,9 @@ class Solution {
     //13:58 ~ 14:08
     public String restoreString(String s, int[] indices) {
         char[] result = new char[indices.length];
-        char[] sArray = s.toCharArray();
         for (int i = 0; i < indices.length; i++) {
-            result[indices[i]] = sArray[i];
+            result[indices[i]] = s.charAt(i);
         }
-        String s1="";
-        for(char sa: result){
-            s1+=sa;
-        }
-        return s1;
+        return new String(result);
     }
 }
